bf887339b3d14c3ae8e76b57a71d4fe4851652ea
alexzakrpl <alexzakr@gmail.com>
2024-06-19 19:23:25 +0200

    Initial commit with customized NvChad configuration by alexzakr

--- init.lua
+++ init.lua
@@ -26,6 +26,18 @@ require("lazy").setup({
   },
 
   { import = "plugins" },
+
+  { "neovim/nvim-lspconfig" },
+  { "hrsh7th/nvim-cmp" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-path" },
+  { "hrsh7th/cmp-cmdline" },
+  { "saadparwaiz1/cmp_luasnip" },
+  { "L3MON4D3/LuaSnip" },
+
+  { "kyazdani42/nvim-tree.lua" },
+  { "kyazdani42/nvim-web-devicons" },
 }, lazy_config)
 
 -- load theme
@@ -37,3 +49,80 @@ require "nvchad.autocmds"
 vim.schedule(function()
   require "mappings"
 end)
+
+-- LSP settings
+local nvim_lsp = require('lspconfig')
+
+-- Используем pyright в качестве LSP сервера для Python
+nvim_lsp.pyright.setup{}
+
+-- Настройки для nvim-cmp
+local cmp = require'cmp'
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    }),
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'buffer' },
+    { name = 'path' },
+  },
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "python",
+  callback = function()
+    vim.opt_local.colorcolumn = "80"
+  end,
+})
+
+-- Настройка nvim-tree.lua
+require("nvim-tree").setup({
+  disable_netrw = true,
+  hijack_netrw = true,
+  open_on_setup = false,
+  ignore_ft_on_setup = {},
+  auto_close = true,
+  open_on_tab = false,
+  hijack_cursor = false,
+  update_cwd = true,
+  lsp_diagnostics = true,
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+  },
+  system_open = {
+    cmd = nil,
+    args = {},
+  },
+  view = {
+    width = 30,
+    side = "left",
+    auto_resize = true,
+    mappings = {
+      custom_only = false,
+      list = {},
+    },
+  },
+  filters = {
+    dotfiles = false,
+    custom = {},
+  },
+})
+
+-- Клавиша для открытия nvim-tree
+vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+
